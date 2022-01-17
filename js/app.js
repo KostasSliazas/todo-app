d3d95c0a326f509c70a953b5019a26e37ef4ed6d
Kostas <kostas.sliazas@gmail.com>
2022-01-17 23:30:11 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,82 @@
 /* eslint-disable no-new */
+const divInstall = document.getElementById('installContainer')
+const butInstall = document.getElementById('butInstall')
+
+/* Put code here */
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent the mini-infobar from appearing on mobile.
+  event.preventDefault()
+  console.log('👍', 'beforeinstallprompt', event)
+  // Stash the event so it can be triggered later.
+  window.deferredPrompt = event
+  // Remove the 'hidden' class from the install button container.
+  divInstall.classList.toggle('hidden', false)
+})
+
+butInstall.addEventListener('click', async () => {
+  console.log('👍', 'butInstall-clicked')
+  const promptEvent = window.deferredPrompt
+  if (!promptEvent) {
+    // The deferred prompt isn't available.
+    return
+  }
+  // Show the install prompt.
+  promptEvent.prompt()
+  // Log the result
+  const result = await promptEvent.userChoice
+  console.log('👍', 'userChoice', result)
+  // Reset the deferred prompt variable, since
+  // prompt() can only be called once.
+  window.deferredPrompt = null
+  // Hide the install button.
+  divInstall.classList.toggle('hidden', true)
+})
+window.addEventListener('appinstalled', (event) => {
+  console.log('👍', 'appinstalled', event)
+  // Clear the deferredPrompt so it can be garbage collected
+  window.deferredPrompt = null
+})
+/* Only register a service worker if it's supported */
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+}
+
+/**
+ * Warn the page must be served over HTTPS
+ * The `beforeinstallprompt` event won't fire if the page is served over HTTP.
+ * Installability requires a service worker with a fetch event handler, and
+ * if the page isn't served over HTTPS, the service worker won't load.
+ */
+if (window.location.protocol === 'http:') {
+  const requireHTTPS = document.getElementById('requireHTTPS')
+  const link = requireHTTPS.querySelector('a')
+  link.href = window.location.href.replace('http://', 'https://')
+  requireHTTPS.classList.remove('hidden')
+}
+
+butInstall.addEventListener('click', async () => {
+  console.log('👍', 'butInstall-clicked')
+  const promptEvent = window.deferredPrompt
+  if (!promptEvent) {
+    // The deferred prompt isn't available.
+    return
+  }
+  // Show the install prompt.
+  promptEvent.prompt()
+  // Log the result
+  const result = await promptEvent.userChoice
+  console.log('👍', 'userChoice', result)
+  // Reset the deferred prompt variable, since
+  // prompt() can only be called once.
+  window.deferredPrompt = null
+  // Hide the install button.
+  divInstall.classList.toggle('hidden', true)
+})
+window.addEventListener('appinstalled', (event) => {
+  console.log('👍', 'appinstalled', event)
+  // Clear the deferredPrompt so it can be garbage collected
+  window.deferredPrompt = null
+})
 new Vue({
   el: '#app',
   data: {
@@ -64,14 +142,13 @@ new Vue({
         }
       })
       // return sum of numbers
-      const sum = numArray.reduce((a, b) => {
+      return numArray.reduce((a, b) => {
         const n1 = a.toString().split('.')[1]
         const n2 = b.toString().split('.')[1]
         const len1 = (n1 && n1.length) || 0
         const len2 = (n2 && n2.length) || 0
         return parseFloat((a + b).toFixed(len1 + len2))
       }, 0)
-      return sum
     }
   }
 })
